47d3b2313b05f9caadf3ddfd6267036327343e6d
Jahvon Dockery <jahvondockery@gmail.com>
2024-03-17 19:41:49 -0400

    Fix build info script

--- internal/cmd/version/get_build_info.sh
+++ internal/cmd/version/get_build_info.sh
@@ -1,16 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+set -ex
 
 current_branch=$(git symbolic-ref -q --short HEAD)
-current_dir=$(pwd)
 
-if [ "$current_branch" == "main" ]; then
+if [ $current_branch = 'main' ]; then
   # If the current branch is main, write the current commit hash, tag or branch name, and build date to the files
-  echo -n $(git rev-parse HEAD) > commit.txt
-  echo -n $(git describe --tags --exact-match 2>/dev/null || git symbolic-ref -q --short HEAD || git rev-parse --short HEAD) &> version.txt
-  echo -n $(git show -z -s --format=%ci) > build_date.txt
+  git rev-parse HEAD > commit.txt
+  version=$(git describe --tags --exact-match 2>/dev/null || git symbolic-ref -q --short HEAD || git rev-parse --short HEAD)
+  echo "$version" &> version.txt
+  git show -z -s --format=%ci > build_date.txt
 else
   # If the current branch is not main, fetch the latest values from the main branch and write those to the files
-  echo -n $(git show main:$current_dir/commit.txt) > commit.txt
-  echo -n $(git show main:$current_dir/version.txt) > version.txt
-  echo -n $(git show main:$current_dir/build_date.txt) > build_date.txt
+  git show main:./commit.txt > commit.txt
+  git show main:./version.txt > version.txt
+  git show main:./build_date.txt > build_date.txt
 fi
