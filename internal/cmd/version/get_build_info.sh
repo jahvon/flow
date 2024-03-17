082c82f3fd508416ec5e1e86b66ce5c6cc68adf7
Jahvon Dockery <jahvondockery@gmail.com>
2024-03-17 09:40:01 -0400

    better handling for version files

--- internal/cmd/version/get_build_info.sh
+++ internal/cmd/version/get_build_info.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-echo -n $(git rev-parse HEAD) > commit.txt
+current_branch=$(git symbolic-ref -q --short HEAD)
+current_dir=$(pwd)
 
-echo -n $(git describe --tags --exact-match 2>/dev/null \
-  || git symbolic-ref -q --short HEAD \
-	|| git rev-parse --short HEAD) &> version.txt
-
-echo -n $(git show -z -s --format=%ci) > build_date.txt
+if [ "$current_branch" == "main" ]; then
+  # If the current branch is main, write the current commit hash, tag or branch name, and build date to the files
+  echo -n $(git rev-parse HEAD) > commit.txt
+  echo -n $(git describe --tags --exact-match 2>/dev/null || git symbolic-ref -q --short HEAD || git rev-parse --short HEAD) &> version.txt
+  echo -n $(git show -z -s --format=%ci) > build_date.txt
+else
+  # If the current branch is not main, fetch the latest values from the main branch and write those to the files
+  echo -n $(git show main:$current_dir/commit.txt) > commit.txt
+  echo -n $(git show main:$current_dir/version.txt) > version.txt
+  echo -n $(git show main:$current_dir/build_date.txt) > build_date.txt
+fi
