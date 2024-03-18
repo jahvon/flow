b2e56a6aa34f631ed43d7cc3a83b992211fecec8
Jahvon Dockery <jahvondockery@gmail.com>
2024-03-17 20:38:58 -0400

    fix

--- internal/cmd/version/get_build_info.sh
+++ internal/cmd/version/get_build_info.sh
@@ -14,8 +14,5 @@ if [ $current_branch = 'main' ]; then
 else
   # If the current branch is not main, fetch the latest values from the main branch and write those to the files
   echo "non-main branch detected, fetching build info from main branch..."
-  git fetch origin main
-  git show main:./commit.txt > commit.txt
-  git show main:./version.txt > version.txt
-  git show main:./build_date.txt > build_date.txt
+  git checkout main -- ./commit.txt ./version.txt ./build_date.txt
 fi
