41af8a4f0d77c5635f1c34f7b1943d679847d2ee
Jahvon Dockery <email@jahvon.dev>
2024-05-20 19:49:10 -0400

    (test) add tests for service packages

---
+++ internal/services/rest/rest_test.go
@@ -0,0 +1,75 @@
+package rest_test
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/jahvon/flow/internal/services/rest"
+)
+
+func TestRest(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Rest Suite")
+}
+
+var _ = Describe("Rest", func() {
+	Context("SendRequest", func() {
+		It("should return error when invalid URL is provided", func() {
+			req := &rest.Request{
+				URL:     "invalid_url",
+				Method:  "GET",
+				Timeout: 30 * time.Second,
+			}
+			_, err := rest.SendRequest(req, []int{http.StatusOK})
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("should return error when unexpected status code is received", func() {
+			req := &rest.Request{
+				URL:     "https://httpbin.org/status/500",
+				Method:  "GET",
+				Timeout: 30 * time.Second,
+			}
+			_, err := rest.SendRequest(req, []int{http.StatusOK})
+			Expect(err).To(Equal(rest.ErrUnexpectedStatusCode))
+		})
+
+		It("should return the correct body when a valid request is made", func() {
+			req := &rest.Request{
+				URL:     "https://httpbin.org/get",
+				Method:  "GET",
+				Timeout: 30 * time.Second,
+			}
+			body, err := rest.SendRequest(req, []int{http.StatusOK})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(body).To(ContainSubstring("\"url\": \"https://httpbin.org/get\""))
+		})
+
+		It("should timeout when the request takes longer than the specified timeout", func() {
+			req := &rest.Request{
+				URL:     "https://httpbin.org/delay/3",
+				Method:  "GET",
+				Timeout: 1 * time.Second,
+			}
+			_, err := rest.SendRequest(req, []int{http.StatusOK})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Client.Timeout exceeded while awaiting headers"))
+		})
+
+		It("should return the correct headers when a valid request is made", func() {
+			req := &rest.Request{
+				URL:     "https://httpbin.org/headers",
+				Method:  "GET",
+				Headers: map[string]string{"Test-Header": "Test-Value"},
+				Timeout: 30 * time.Second,
+			}
+			body, err := rest.SendRequest(req, []int{http.StatusOK})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(body).To(ContainSubstring("\"Test-Header\": \"Test-Value\""))
+		})
+	})
+})
---
+++ internal/services/run/run_test.go
@@ -0,0 +1,136 @@
+package run_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	tuikitIO "github.com/jahvon/tuikit/io"
+	"github.com/jahvon/tuikit/io/mocks"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+
+	"github.com/jahvon/flow/internal/services/run"
+)
+
+func TestRun(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Run Suite")
+}
+
+var _ = Describe("Run", func() {
+	var (
+		ctrl   *gomock.Controller
+		logger *mocks.MockLogger
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		logger = mocks.NewMockLogger(ctrl)
+		logger.EXPECT().Debugf(gomock.Any(), gomock.Any()).AnyTimes()
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	Describe("RunCmd", func() {
+		When("log mode is hidden", func() {
+			It("should not log the command output", func() {
+				logger.EXPECT().SetMode(gomock.Any()).AnyTimes()
+				logger.EXPECT().LogMode().DoAndReturn(func() tuikitIO.LogMode {
+					return tuikitIO.Hidden
+				}).AnyTimes()
+				err := run.RunCmd("echo \"foo\"", "", nil, tuikitIO.Hidden, logger, os.Stdin, nil)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+		When("log mode is text", func() {
+			It("should log the command output", func() {
+				logger.EXPECT().SetMode(gomock.Any()).AnyTimes()
+				logger.EXPECT().LogMode().DoAndReturn(func() tuikitIO.LogMode {
+					return tuikitIO.Text
+				}).AnyTimes()
+				logger.EXPECT().Println("foo").Times(1)
+				err := run.RunCmd("echo \"foo\"", "", nil, tuikitIO.Text, logger, os.Stdin, nil)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+		When("log mode is logfmt", func() {
+			It("should log the command output", func() {
+				logger.EXPECT().SetMode(gomock.Any()).AnyTimes()
+				logger.EXPECT().LogMode().DoAndReturn(func() tuikitIO.LogMode {
+					return tuikitIO.Logfmt
+				}).AnyTimes()
+				logger.EXPECT().Infof("foo", gomock.Any()).Times(1)
+				err := run.RunCmd("echo \"foo\"", "", nil, tuikitIO.Logfmt, logger, os.Stdin, nil)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+		When("log mode is json", func() {
+			It("should log the command output", func() {
+				logger.EXPECT().SetMode(gomock.Any()).AnyTimes()
+				logger.EXPECT().LogMode().DoAndReturn(func() tuikitIO.LogMode {
+					return tuikitIO.JSON
+				}).AnyTimes()
+				logger.EXPECT().Infof("foo", gomock.Any()).Times(1)
+				err := run.RunCmd("echo \"foo\"", "", nil, tuikitIO.JSON, logger, os.Stdin, nil)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+		When("log fields are provided", func() {
+			It("should log the command output with the log fields", func() {
+				logger.EXPECT().SetMode(gomock.Any()).AnyTimes()
+				logger.EXPECT().LogMode().DoAndReturn(func() tuikitIO.LogMode {
+					return tuikitIO.Logfmt
+				}).AnyTimes()
+				fields := map[string]interface{}{"key": "value"}
+				logger.EXPECT().Infox("foo", "key", "value").Times(1)
+				err := run.RunCmd("echo \"foo\"", "", nil, tuikitIO.JSON, logger, os.Stdin, fields)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+		When("env vars are provided", func() {
+			It("should log the command output with the env vars", func() {
+				logger.EXPECT().SetMode(gomock.Any()).AnyTimes()
+				logger.EXPECT().LogMode().DoAndReturn(func() tuikitIO.LogMode {
+					return tuikitIO.Logfmt
+				}).AnyTimes()
+				env := []string{"key=value"}
+				logger.EXPECT().Infof("value", gomock.Any()).Times(1)
+				err := run.RunCmd("echo \"$key\"", "", env, tuikitIO.JSON, logger, os.Stdin, nil)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("RunFile", func() {
+		var testfile *os.File
+
+		BeforeEach(func() {
+			var err error
+			testfile, err = os.CreateTemp("", "test.sh")
+			Expect(err).NotTo(HaveOccurred())
+			_, err = testfile.WriteString("#!/bin/sh\necho foo")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			Expect(testfile.Close()).To(Succeed())
+			Expect(os.Remove(testfile.Name())).To(Succeed())
+		})
+
+		It("should log the file execution output", func() {
+			logger.EXPECT().SetMode(gomock.Any()).AnyTimes()
+			logger.EXPECT().LogMode().DoAndReturn(func() tuikitIO.LogMode {
+				return tuikitIO.Text
+			}).AnyTimes()
+			logger.EXPECT().Println("foo").Times(1)
+			filename := filepath.Base(testfile.Name())
+			filedir := filepath.Dir(testfile.Name())
+			err := run.RunFile(filename, filedir, nil, tuikitIO.Logfmt, logger, os.Stdin, nil)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
