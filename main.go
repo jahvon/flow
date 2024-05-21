a1799717940bb90a5b44864c9774d598b2a94ae1
Jahvon Dockery <email@jahvon.dev>
2024-05-20 22:20:23 -0400

    (refactor) change root cmd main.go execution

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	stdCtx "context"
+	"os"
 
 	"github.com/jahvon/flow/cmd"
 	"github.com/jahvon/flow/internal/context"
 )
 
 func main() {
-	ctx := context.NewContext(stdCtx.Background())
+	ctx := context.NewContext(stdCtx.Background(), os.Stdin, os.Stdout)
 	defer ctx.Finalize()
 
 	if ctx == nil {
 		panic("failed to initialize context")
 	}
+	rootCmd := cmd.NewRootCmd(ctx)
 	ctx.Ctx, ctx.CancelFunc = stdCtx.WithCancel(ctx.Ctx)
-	cmd.Execute(ctx)
+	if err := cmd.Execute(ctx, rootCmd); err != nil {
+		ctx.Logger.FatalErr(err)
+	}
 }
